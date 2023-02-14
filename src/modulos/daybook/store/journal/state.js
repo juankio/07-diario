0ce8da988fad29acbbcef81949aefa3f3eb25fd8
juankio <74984894+juankio@users.noreply.github.com>
2023-02-14 18:56:19 -0500

    xd

--- src/modulos/daybook/store/journal/state.js
+++ src/modulos/daybook/store/journal/state.js
@@ -15,7 +15,8 @@ export default ()=>({
         date:new Date().toDateString(),
         text:'Estos textos hacen parecerlo un ssssssssssssespañol que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, ',
         picture:null,
-        }
+        },
+        
         {
         id:new Date().getTime() + 2000,
         date:new Date().toDateString(),
